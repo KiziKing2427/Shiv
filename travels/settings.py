e82d1a5c3b613671b93092c9646ec9e8c1e56504
kiziKing2427 <ugofabian7@gmail.com>
2024-04-08 18:04:09 +0700

    commit13

--- travels/settings.py
+++ travels/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, 'static/dist'),
+     os.path.join(BASE_DIR, 'static/src/assests'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
