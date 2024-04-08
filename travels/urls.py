dee4128dee715c67af1b53edcfeba5c24cb0afaa
kiziKing2427 <ugofabian7@gmail.com>
2024-04-08 20:33:55 +0700

    commit23

--- travels/urls.py
+++ travels/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/', include('ytriTravel.urls')),  
-    path('', TemplateView.as_view(template_name='dist/index.html')),
-
+    path('home/', TemplateView.as_view(template_name='home.html')),
+    re_path('.*', TemplateView.as_view(template_name='dist/index.html'))
 ]
 
 
