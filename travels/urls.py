5886b539521dba94d2dba68a6895391bf84e499b
kiziKing2427 <ugofabian7@gmail.com>
2024-04-08 17:49:49 +0700

    commit11

--- travels/urls.py
+++ travels/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include
+from django.urls import path, include, re_path
 from ytriTravel.views import CreateAccountView, ProductView
 from rest_framework.routers import DefaultRouter
+from django.views.generic import TemplateView
 
 router = DefaultRouter()
 router.register(r'create-account', CreateAccountView, basename='create_account')
@@ -12,8 +13,10 @@ router.register(r'product', ProductView, basename='product')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api/', include('ytriTravel.urls')),  # Add this line to include the other URLs
+    path('api/', include('ytriTravel.urls')),  
+    re_path('.*', TemplateView.as_view(template_name='index.html'))
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
