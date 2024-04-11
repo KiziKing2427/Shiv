57e4fca9d96453c9373e52daf6f427be96de90c6
kiziKing2427 <ugofabian7@gmail.com>
2024-04-11 12:49:29 +0700

    commit changes14

--- travels/settings.py
+++ travels/settings.py
@@ -103,13 +103,10 @@ WSGI_APPLICATION = 'travels.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ytri',
-        'USER': 'kizi',
-        'PASSWORD': 'Courage_2427123',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
