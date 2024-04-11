2cde0378770abd3407cad535bffcf1b40fe5780c
kiziKing2427 <ugofabian7@gmail.com>
2024-04-11 12:25:24 +0700

    commit changes10

--- travels/settings.py
+++ travels/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -102,13 +103,9 @@ WSGI_APPLICATION = 'travels.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# Update database configuration from DATABASE_URL environment variable
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ytri',
-        'USER': 'kizi',
-        'PASSWORD': 'Courage_2427123',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
